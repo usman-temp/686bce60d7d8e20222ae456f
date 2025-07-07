846939d357a5aa9836f78db4a1b2674379ac0955
usman-temp <usmanjaved1620@gmail.com>
2025-07-07 19:05:06 +0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-geist-sans)'],
+        mono: ['var(--font-geist-mono)']
+      },
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)'
+      }
+    },
+  },
+  plugins: [],
+}
